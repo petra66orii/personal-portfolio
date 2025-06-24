24d0143a2cab3c68ea6cc9901d15332ee889e8bf
Petra <petra66orii@gmail.com>
2025-06-24 20:25:27 +0100

    Create models and migrate

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    repo_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+    tech_stack = models.CharField(max_length=255)
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    # Level: Beginner, Intermediate, Expert
+    level = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
