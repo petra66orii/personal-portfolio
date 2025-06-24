638d3a6bb20f43cebf7583d204388eafd6b57b94
Petra <petra66orii@gmail.com>
2025-06-24 20:35:26 +0100

    Create contact messge model and migrate

--- portfolio/models.py
+++ portfolio/models.py
@@ -21,3 +21,13 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name}"
