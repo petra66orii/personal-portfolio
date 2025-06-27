c71824356e172b38efff7f923b435cd8a7d47162
Petra <petra66orii@gmail.com>
2025-06-27 14:53:30 +0100

    Add color palette

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,16 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 
-type Project = {
-  id: number;
-  title: string;
-  description: string;
-  tech_stack: string;
-  repo_link?: string;
-  live_link?: string;
-  featured: boolean;
-};
-
 function App() {
-  const [projects, setProjects] = useState<Project[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/projects/")
-      .then((res) => res.json())
-      .then((data) => setProjects(data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <>
       <Navbar />
-      <main className="bg-white text-black dark:bg-gray-900 dark:text-white min-h-screen p-6">
+      <main className="min-h-screen">
         <Routes>
-          <Route path="/" element={<Home projects={projects} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
