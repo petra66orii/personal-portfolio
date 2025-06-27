76253c4ad0c4e4d344cb8d230ae9ec9dd6f8f595
Petra <petra66orii@gmail.com>
2025-06-27 18:31:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env bash
+
+# install dependencies
+pip install -r requirements.txt
+
+# collect static files
 python manage.py collectstatic --noinput
+
+# run database migrations
 python manage.py migrate
