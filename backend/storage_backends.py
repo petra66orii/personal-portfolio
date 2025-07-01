0e5175bfa4f81a23c1bc60a11bdba1b075873e25
Petra <petra66orii@gmail.com>
2025-07-01 16:44:09 +0100

    Correct bucket name

--- backend/storage_backends.py
+++ backend/storage_backends.py
@@ -2,5 +2,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = 'personal-portfolio'
+    bucket_name = 'personal-portfolio-media-bucket'
     file_overwrite = False
