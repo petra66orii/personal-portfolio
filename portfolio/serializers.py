fb14d423f9be2e35f7b60c4104a358877d7ef363
Petra <petra66orii@gmail.com>
2025-07-01 18:35:41 +0100

    .

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -11,13 +11,20 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         try:
-            url = obj.image.url
-            if url.startswith('http'):
-                return url
+            # Get the raw field value instead of .url to avoid MEDIA_URL prepending
+            image_value = obj.image.name if obj.image else None
+            if not image_value:
+                return None
+
+            # If it's already a full URL, return it as-is
+            if image_value.startswith('http'):
+                return image_value
+
+            # Otherwise, build the full URL for relative paths
             request = self.context.get('request')
             if request:
-                return request.build_absolute_uri(url)
-            return url
+                return request.build_absolute_uri(f'/media/{image_value}')
+            return f'/media/{image_value}'
         except Exception:
             return None
 
