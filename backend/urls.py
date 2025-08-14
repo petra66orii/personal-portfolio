9fc5fce92983ac41e370f0343e982797ec8767e8
Petra <petra66orii@gmail.com>
2025-08-14 20:13:20 +0100

    .

--- backend/urls.py
+++ backend/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-    
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Serve static files
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Redirect all other paths to index.html
+urlpatterns += [
+    re_path(r'^(?!static/).*$', TemplateView.as_view(template_name="index.html")),
+]
