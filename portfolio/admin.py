ee5e114f67581ef65bbf35de5c46e6a351d45f62
Petra <petra66orii@gmail.com>
2025-08-22 23:06:22 +0100

    Register Service models with admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from .models import Project, Skill, ContactMessage, BlogPost
+from .models import Project, Skill, ContactMessage, BlogPost, Service, ServiceInquiry
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Skill)
 admin.site.register(ContactMessage)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'service_type', 'featured', 'active', 'order']
+    list_filter = ['service_type', 'featured', 'active']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['order', 'name']
+    list_editable = ['featured', 'active', 'order']
+
+
+@admin.register(ServiceInquiry)
+class ServiceInquiryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'service', 'budget_range', 'timeline', 'created_at', 'responded']
+    list_filter = ['service', 'budget_range', 'timeline', 'responded', 'created_at']
+    search_fields = ['name', 'email', 'company', 'project_details']
+    readonly_fields = ['created_at']
+    list_editable = ['responded']
+    date_hierarchy = 'created_at'
+    ordering = ['-created_at']
 
 
 @admin.register(BlogPost)
