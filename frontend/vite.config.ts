dd2587d6cdd13bd6de9664fbeb9d99b52f14a484
Petra <petra66orii@gmail.com>
2025-08-27 20:59:07 +0100

    Fix backend connection to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ command }) => {
+  const plugins = [
     react(),
-    tailwindcss()
-  ],
+    tailwindcss(),
+  ];
+
+  if (command === 'serve') {
+    // --- DEVELOPMENT ---
+    return {
+      plugins,
+      base: '/',
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      },
+    }
+  } else {
+    // --- PRODUCTION BUILD ---
+    return {
+      plugins,
+      base: '/static/',
+    }
+  }
 })
