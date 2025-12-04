c11f548ab341a122fb7048f4bd0177a67d041ab3
Petra <petra66orii@gmail.com>
2025-12-04 20:49:45 +0000

    Change static and template settings

--- backend/settings.py
+++ backend/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "frontend/dist"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -157,6 +157,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist'),
+    os.path.join(BASE_DIR, 'frontend', 'dist-admin'), 
 ]
     # For local development, no additional staticfiles dirs needed
 
