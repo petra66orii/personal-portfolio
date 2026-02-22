7e086f7f4006b9e54910ec227cd1a4b8dd10967b
Miss Bott <developer@missbott.online>
2026-02-22 18:14:27 +0000

    Update email services

--- backend/settings.py
+++ backend/settings.py
@@ -215,7 +215,7 @@ CELERY_TIMEZONE = "Europe/Dublin"
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
